9db7098553f343c1eede94b927fa4435cadc421f
Prateek195jain <118386573+Prateek195jain@users.noreply.github.com>
2026-07-25 17:09:06 +0530

    Configure CORS for Vercel frontend

--- backend/page-pulse/page-pulse/src/main/java/com/prateek/pagepulse/config/WebConfig.java
+++ backend/page-pulse/page-pulse/src/main/java/com/prateek/pagepulse/config/WebConfig.java
@@ -6,19 +6,20 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig {
+public class CorsConfig {
 
     @Bean
     public WebMvcConfigurer corsConfigurer() {
-
         return new WebMvcConfigurer() {
 
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-
-                registry.addMapping("/api/**")
-                        .allowedOrigins("http://localhost:5173")
-                        .allowedMethods("*")
+                registry.addMapping("/**")
+                        .allowedOrigins(
+                                "http://localhost:5173",
+                                "https://page-pulse-tan-ten.vercel.app"
+                        )
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*");
             }
         };
